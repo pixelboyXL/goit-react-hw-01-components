87df24c042e0cea9b4c24aa3e8b66064af7136a4
Dmytro Kravchenko <dmitrij.kravchenko.1997@gmail.com>
2022-08-11 14:21:54 +0300

    v. 1.3.1

--- src/components/FriendList/FriendListItem.js
+++ src/components/FriendList/FriendListItem.js
@@ -2,8 +2,8 @@ import { FriendsListItemStyle } from 'components/FriendList/FriendList.styled';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
     return (
-        <FriendsListItemStyle>
-            <span>{isOnline}</span>
+        <FriendsListItemStyle isOnline={isOnline}>
+            <span></span>
             <img src={avatar} alt="User avatar" width="48" />
             <p>{name}</p>
         </FriendsListItemStyle>
